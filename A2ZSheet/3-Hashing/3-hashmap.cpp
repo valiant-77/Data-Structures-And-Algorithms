9920f5500da8be459e00b64748492d0a2f0e1269
valiant-77 <adityagirish2324@gmail.com>
2024-10-09 22:35:51 +0530

    Add files via upload

---
+++ A2ZSheet/3-Hashing/3-hashmap.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the size of the arrray: ";
+    cin>>n;
+    int arr[n];
+    
+    cout<<"Enter the array elements: ";
+    
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    unordered_map<int,int>mp;
+    for(int i=0;i<n;i++)
+    {
+        mp[arr[i]]++;
+    }
+
+        
+    
+    
+    
+    
+    int q;
+    cout<<"Enter the number of numbers to be searched: ";
+    cin>>q;
+    
+    while(q--)
+    {
+        int num;
+        cout<<"Enter the number to be searched: ";
+        cin>>num;
+        
+        cout<<"The occurence of "<<num<<" is "<<mp[num]<<" times"<<endl;
+    }
+
+    return 0;
+}
