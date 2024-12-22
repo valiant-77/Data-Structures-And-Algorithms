c1a5df7fc804bbbd80f1c8f071c4dd8dc5e8b81d
valiant-77 <adityagirish2324@gmail.com>
2024-12-22 19:43:58 +0530

    Add files via upload

---
+++ A2ZSheet/9-LinkedLists/4-SLL(FAQ)/Q-MiddlenodeOP.cpp
@@ -0,0 +1,84 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Define a Node structure
+class Node {
+    public:
+    int data;
+    Node* next;
+
+    
+    // Constructor to initialize a new node
+    public:
+    Node(int d,Node* n,Node* p)
+    {
+        data=d;
+        next=n;
+      
+    }
+    
+    public:
+    Node(int d){
+        data=d;
+        next=nullptr;
+        
+    }
+};
+    
+
+
+// Function to convert an array to a linked list
+Node* arrayToLinkedList(int arr[], int size) {
+    if (size == 0) return nullptr;
+
+    // Create head of the linked list
+    Node* head = new Node(arr[0]);
+    Node* current = head;
+
+   /* Iterate through the array 
+   and create linked list nodes*/
+    for (int i = 1; i < size; i++) {
+        Node *temp= new Node(arr[i]);
+        current->next=temp;
+        current=current->next;
+    }
+
+    return head;
+}
+
+int mid(Node* head) {
+        Node* slow=head;
+        Node*fast=head;
+
+        while(fast!=nullptr && fast->next!=nullptr)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+
+        return slow->data;
+
+        
+    }
+
+
+
+
+int main() {
+    int arr[] = {1,2,3,4,5,6,7,8};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    // Convert array to linked list
+    Node* head = arrayToLinkedList(arr, size);
+
+cout<<mid(head);
+    /*Node* temp = x;
+    while (temp != nullptr) {
+        cout << temp->data << " -> ";
+       temp = temp->next;
+    }
+    cout << "nullptr" << endl;*/
+
+
+    return 0;
+}
