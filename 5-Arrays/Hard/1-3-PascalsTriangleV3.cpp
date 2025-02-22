087010ebca5ce42e3c240a42a731e0fdb42f4971
valiant-77 <adityagirish2324@gmail.com>
2025-02-22 12:16:16 +0530

    Update 1-3-PascalsTriangleV3.cpp

--- 5-Arrays/Hard/1-3-PascalsTriangleV3.cpp
+++ 5-Arrays/Hard/1-3-PascalsTriangleV3.cpp
@@ -1,15 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int x = 5;  // Number of rows in Pascal's Triangle
+int main() 
+{
+    int n = 5;  // Number of rows in Pascal's Triangle
 
     // Iterate through each row
-    for (int row = 0; row < x; row++) {
+    for (int row = 0; row < n; row++) 
+{
         int value = 1;  // The first value in each row is always 1
         
         // Print all values in the current row using binomial coefficients
-        for (int i = 0; i <= row; i++) {
+        for (int i = 0; i <= row; i++) 
+       {
             cout << value << " ";  // Output the current value
             
             // Update the value for the next element in the row
