0f3e3d12cfc15748256303ad50cbfcd1c07ce625
Valiant-77 <you@example.com>
2025-04-06 18:02:32 +0530

    Notes

--- 2-Recursion/1-1toN.cpp
+++ 2-Recursion/1-1toN.cpp
@@ -1,3 +1,20 @@
+/*
+Logic:
+1-Get the number n
+2-keep calling f(n-1) till n becomes 0
+3-when n becomes 0 the flow returns to its calling function  and resumes next steps
+n=5
+5-4-3-2-1-0 ->1 2 3 4 5
+
+
+TC=O(n)
+sc=o(n)
+
+*/
+
+
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
